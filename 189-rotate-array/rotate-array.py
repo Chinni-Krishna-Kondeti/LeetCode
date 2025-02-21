f261d2e5903bc286c664a96605e8f3739fa23b55
Chinni-Krishna-Kondeti <kchinni155@gmail.com>
2025-02-21 14:29:40 -0600

    Time: 3 ms (71.37%) | Memory: 25.7 MB (46.27%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,12 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        n = len(nums)
-        k = k % n
-        temp = nums[-k::]
-        for j in range(n-1-k, -1, -1):
-            nums[j], nums[j+k] = nums[j+k], nums[j]
-        for j in range(k):
-            nums[j] = temp[j]
-        
+        k = k % len(nums)
+        nums[:] = nums[-k::] + nums[:-k:]
+        print(nums)
