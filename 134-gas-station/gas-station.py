f6a9f681c63aa995ca13394022736f4a2ddcc5bd
Chinni-Krishna-Kondeti <kchinni155@gmail.com>
2025-02-24 21:43:37 -0600

    Time: 16 ms (80.63%) | Memory: 23.4 MB (22.30%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,7 +1,6 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        diff = list(map(lambda x, y: x - y, gas, cost))
-        if -sum(filter(lambda x: x < 0, diff)) > sum(filter(lambda x: x > 0, diff)):
+        if sum(cost) > sum(gas):
             return -1
         n = len(gas)
         cum_tank_diff = 0
@@ -11,7 +10,5 @@ class Solution:
             if cum_tank_diff <= 0:
                 start_index = i + 1
                 cum_tank_diff = 0
-        if start_index is None:
-            return -1
         start_index = start_index % n
         return start_index
